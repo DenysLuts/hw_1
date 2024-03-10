c522f17f7470a3cb71b7636680c6526f7c454f21
DenysLuts <159810051+DenysLuts@users.noreply.github.com>
2024-03-10 20:25:46 +0100

    homework_1

---
+++ hw_1.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+def get_days_from_today(date):
+    try:
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        today = datetime.today()
+        delta = today - date_obj
+        return delta.days
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте формат 'РРРР-ММ-ДД'."
+print(get_days_from_today("2006-10-09"))
